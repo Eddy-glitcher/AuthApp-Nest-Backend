107c3d9493da754e1fab2a33eb8b58fd972d6e9d
Eddy-glitcher <luis-gonzales6@hotmail.com>
2024-01-09 18:25:10 -0500

    Feat: Added railway backend optimizations

--- .env.template.ts
+++ .env.template.ts
@@ -1,4 +1,5 @@
 
 
-MONGO_URI = mongodb://localhost:27017/mean-db
+MONGO_URI = mongodb://mongo:2EHaH4F4Bf65D-Cbd261bfacACbd41C-@roundhouse.proxy.rlwy.net:20364
+MONGO_DB_NAME = AuthDb
 JWT_SEED = toobussydreaming123456
